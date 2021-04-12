23e7c98c2cb3a15424a5c41fde77310c0ec88ec5
Can Bakiskan <canbakiskan@ucsb.edu>
2021-04-12 01:06:17 -0700

    fixed .nonzero warning

--- deepillusion/torchattacks/_pgd.py
+++ deepillusion/torchattacks/_pgd.py
@@ -133,7 +133,8 @@ def PGD(net, x, y_true, data_params, attack_params, loss_function="cross_entropy
                                      data_params["x_min"], data_params["x_max"]))
             y_hat = output.argmax(dim=1, keepdim=True)
 
-            fooled_indices = (y_true != y_hat.view_as(y_true)).nonzero().squeeze()
+            fooled_indices = (y_true != y_hat.view_as(
+                y_true)).nonzero(as_tuple=False).squeeze()
             best_perturbation[fooled_indices] = perturbation[fooled_indices].data
 
     # set back to saved values
@@ -266,7 +267,8 @@ def PGD_EOT(net, x, y_true, data_params, attack_params, loss_function="cross_ent
                                      data_params["x_min"], data_params["x_max"]))
             y_hat = output.argmax(dim=1, keepdim=True)
 
-            fooled_indices = (y_true != y_hat.view_as(y_true)).nonzero().squeeze()
+            fooled_indices = (y_true != y_hat.view_as(
+                y_true)).nonzero(as_tuple=False).squeeze()
             best_perturbation[fooled_indices] = perturbation[fooled_indices].data
 
     # set back to saved values
@@ -403,7 +405,8 @@ def PGD_EOT_normalized(net, x, y_true, data_params, attack_params, loss_function
                                      data_params["x_min"], data_params["x_max"]))
             y_hat = output.argmax(dim=1, keepdim=True)
 
-            fooled_indices = (y_true != y_hat.view_as(y_true)).nonzero().squeeze()
+            fooled_indices = (y_true != y_hat.view_as(
+                y_true)).nonzero(as_tuple=False).squeeze()
             best_perturbation[fooled_indices] = perturbation[fooled_indices].data
 
     # set back to saved values
@@ -534,7 +537,16 @@ def PGD_EOT_sign(net, x, y_true, data_params, attack_params, loss_function="cros
                                      data_params["x_min"], data_params["x_max"]))
             y_hat = output.argmax(dim=1, keepdim=True)
 
-            fooled_indices = (y_true != y_hat.view_as(y_true)).nonzero().squeeze()
+            fooled_indices = (y_true != y_hat.view_as(
+                y_true)).nonzero(as_tuple=False).squeeze()
+            best_perturbation[fooled_indices] = perturbation[fooled_indices].data
+
+    # set back to saved values
+    for i, p in enumerate(net.parameters()):
+        p.requires_grad = requires_grad_save[i]
+
+    return best_perturbation
+
             best_perturbation[fooled_indices] = perturbation[fooled_indices].data
 
     # set back to saved values
