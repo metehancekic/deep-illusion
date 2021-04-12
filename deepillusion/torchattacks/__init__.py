3820d02f5aab51d674ad0f54971fb561f139683e
Can Bakiskan <canbakiskan@ucsb.edu>
2021-04-12 01:06:47 -0700

    added PGD_smooth

--- deepillusion/torchattacks/__init__.py
+++ deepillusion/torchattacks/__init__.py
@@ -2,13 +2,13 @@
 
 
 from ._fgsm import FGSM, FGSM_targeted, FGM
-from ._rfgsm import RFGSM
-from ._pgd import PGD, PGD_EOT, PGD_EOT_normalized, PGD_EOT_sign
+from ._rfgsm import RFGSM, RFGM
+from ._pgd import PGD, PGD_EOT, PGD_EOT_normalized, PGD_EOT_sign, PGD_smooth
 from ._bim import BIM, BIM_EOT
 from ._spsa import SPSA
 from ._soft_attacks import soft_attack_single_step, iterative_soft_attack
 from .._version import __version__
 
-__all__ = ["FGSM", "FGSM_targeted", "FGM", "RFGSM", "PGD", "PGD_EOT", "PGD_EOT_normalized",
+__all__ = ["FGSM", "FGSM_targeted", "FGM", "RFGSM", "RFGM", "PGD", "PGD_EOT", "PGD_smooth", "PGD_EOT_normalized",
                    "PGD_EOT_sign", "BIM", "BIM_EOT", "SPSA", "soft_attack_single_step",
                    "iterative_soft_attack", "__version__"]
